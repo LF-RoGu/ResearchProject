ab50832c04c0a417dfde604bafe45f95f34ee342
LF-RoGu <luis_rogu@outlook.com>
2025-06-20 06:58:32 -0400

    push for debug

--- 03_Code/02_C++/06_xdaSensorSendRequestThreadClassbased/main.d
+++
@@ -1,8 +0,0 @@
-main.o: main.cpp mti_utility.h xsens_mti710.hpp libs/xsens_mti.h \
- libs/xsens_constants.h libs/xsens_constants.h libs/xsens_utility.h
-mti_utility.h:
-xsens_mti710.hpp:
-libs/xsens_mti.h:
-libs/xsens_constants.h:
-libs/xsens_constants.h:
-libs/xsens_utility.h:
--- 03_Code/02_C++/06_xdaSensorSendRequestThreadClassbased/xsens_mti710.d
+++
@@ -1,9 +0,0 @@
-xsens_mti710.o: xsens_mti710.cpp xsens_mti710.hpp mti_utility.h \
- libs/xsens_mti.h libs/xsens_constants.h libs/xsens_constants.h \
- libs/xsens_utility.h
-xsens_mti710.hpp:
-mti_utility.h:
-libs/xsens_mti.h:
-libs/xsens_constants.h:
-libs/xsens_constants.h:
-libs/xsens_utility.h:
