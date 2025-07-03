6fa44ef51b7db8125172997aea76c0df782bfdd2
LF-RoGu <luis_rogu@outlook.com>
2025-07-03 07:34:33 -0400

    debug logic works, fixing sending to main file

--- 03_Code/02_C++/01_mmWave-IWR6843/01_readMmwave/radar_sensor/IWR6843.d
+++
@@ -1,9 +0,0 @@
-radar_sensor/IWR6843.o: radar_sensor/IWR6843.cpp radar_sensor/IWR6843.h \
- radar_sensor/SensorData.h radar_sensor/sensor_data/UARTFrame.h \
- radar_sensor/sensor_data/lib/SensorDataStr.h \
- radar_sensor/sensor_data/lib/EndianUtils.h
-radar_sensor/IWR6843.h:
-radar_sensor/SensorData.h:
-radar_sensor/sensor_data/UARTFrame.h:
-radar_sensor/sensor_data/lib/SensorDataStr.h:
-radar_sensor/sensor_data/lib/EndianUtils.h:
