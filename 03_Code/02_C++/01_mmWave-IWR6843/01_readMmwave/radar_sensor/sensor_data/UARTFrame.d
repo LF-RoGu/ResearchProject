304b6c3a9323a95cd27acb878686ceb3b7e074f7
LF-RoGu <luis_rogu@outlook.com>
2025-07-03 05:57:14 -0400

    Obtained sane data from radar sensor

--- 03_Code/02_C++/01_mmWave-IWR6843/01_readMmwave/radar_sensor/sensor_data/UARTFrame.d
+++
@@ -1,8 +0,0 @@
-radar_sensor/sensor_data/UARTFrame.o: \
- radar_sensor/sensor_data/UARTFrame.cpp \
- radar_sensor/sensor_data/UARTFrame.h \
- radar_sensor/sensor_data/lib/SensorDataStr.h \
- radar_sensor/sensor_data/lib/EndianUtils.h
-radar_sensor/sensor_data/UARTFrame.h:
-radar_sensor/sensor_data/lib/SensorDataStr.h:
-radar_sensor/sensor_data/lib/EndianUtils.h:
