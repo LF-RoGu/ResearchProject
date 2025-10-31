16c7ae3c5fa40d274103dbf238af774e14af6753
Potato <luis_rogu@outlook.com>
2025-10-31 11:57:44 +0100

    code updates

--- 03_Code/01_Python/06_sensorFusion/03_Code-qtViewLogs/Pipeline.py
+++ 03_Code/01_Python/06_sensorFusion/03_Code-qtViewLogs/Pipeline.py
@@ -74,8 +74,8 @@ imu_heading_rad = None
 T_global = np.eye(3)  # initial pose at origin
 
 
-folderName = "14_outside"  # Folder where CSV files are stored
-testType = "outside6.csv"  # Type of test data
+folderName = "16_presentation/01_backupPresentation"  # Folder where CSV files are stored
+testType = "presentationTest.csv"  # Type of test data
 # Instantiate readers and global aggregators
 radarLoaderA = RadarCSVReader("radarA_" + testType, folderName) if ENABLE_SENSORS in (1, 3) else None
 radarLoaderB = RadarCSVReader("radarB_" + testType, folderName) if ENABLE_SENSORS in (1, 3) else None
